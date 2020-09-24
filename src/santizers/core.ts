22aa023fffb3c1dbcf475d01f3d6814cb656aa72
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-09-24 18:12:58 +0100

    Added tests for sanitizers core

--- src/santizers/core.ts
+++ src/santizers/core.ts
@@ -1,9 +1,21 @@
-export const capitalizeText = (text: string) => `${text[0].toUpperCase()}${text.slice(1).toLowerCase()}`
+export const capitalizeText = (text: string) => {
+	if(text === null || text === undefined) return text
+	text = text.toString()
+	return `${text?.[0].toUpperCase()}${text?.slice(1)?.toLowerCase()}`
+}
 
-export const extractTextFromHTML = (html: string) => html?.trim().replace(/<[^>]+>/g, '') ?? ''
+export const extractTextFromHTML = (html: string) => {
+	if(html === null || html === undefined) return html
+	return html?.toString()?.trim().replace(/<[^>]+>/g, '') ?? ''
+}
 
 export const trimToLength = (body: string, length: number) => {
-	if(body.length < length){ return body }
-	const index = body.indexOf(' ', length)
-	return `${body.slice(0, index)}...`
+	if(body === null || body === undefined) return body
+
+	body = body.toString()
+	if(body.length < length) return body
+
+	const indexOfSpace = body.indexOf(' ', length)
+	const indexToTrim = indexOfSpace === -1 ? length : indexOfSpace
+	return `${body.slice(0, indexToTrim)}...`
 }
---
+++ tests/sanitizers/core.test.ts
@@ -0,0 +1,33 @@
+import { capitalizeText, extractTextFromHTML, trimToLength } from '../../src/santizers'
+
+test('CapitalizeText', () => {
+	expect(capitalizeText('test')).toBe('Test')
+	expect(capitalizeText(2 as unknown as string)).toBe('2')
+	expect(capitalizeText(null as unknown as string)).toBe(null)
+	expect(capitalizeText(undefined as unknown as string)).toBe(undefined)
+})
+
+test('ExtractTextFromHTML', () => {
+	expect(extractTextFromHTML('test')).toBe('test')
+	expect(extractTextFromHTML(2 as unknown as string)).toBe('2')
+
+	expect(extractTextFromHTML('<p>a</p>')).toBe('a')
+	expect(extractTextFromHTML('<p>a<a>b</a></p>')).toBe('ab')
+	expect(extractTextFromHTML('<p>a<img src="/" /></p>')).toBe('a')
+
+	expect(extractTextFromHTML(null as unknown as string)).toBe(null)
+	expect(extractTextFromHTML(undefined as unknown as string)).toBe(undefined)
+})
+
+test('TrimToLength', () => {
+	expect(trimToLength('abc', 10)).toBe('abc')
+	expect(trimToLength('abc', 1)).toBe('a...')
+
+	expect(trimToLength(null as unknown as string, 1)).toBe(null)
+	expect(trimToLength(undefined as unknown as string, 1)).toBe(undefined)
+
+	const num = 123, arr = [1,2,3], obj = { name: 'test' }
+	expect(trimToLength(num as unknown as string, 10)).toBe(num.toString())
+	expect(trimToLength(arr as unknown as string, 10)).toBe(arr.toString())
+	expect(trimToLength(obj as unknown as string, 20)).toBe(obj.toString())
+})
