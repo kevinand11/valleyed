27be336ed4f17849e67dfe799ed8d346ab682415
kevinand11 <kevinfizu@gmail.com>
2022-05-21 16:24:02 +0100

    fix: remove requiredIf

--- src/utils/helpers/core.ts
+++
@@ -1,5 +0,0 @@
-import { isRequiredIf } from '../../rules'
-
-export function isRequiredIfX<Type> (condition: boolean, error?: string) {
-	return (val: Type) => isRequiredIf(val, condition, error)
-}
