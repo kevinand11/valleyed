663772a2c31f938a360ba739cfe608f4b1ad8357
kevinand11 <kevinfizu@gmail.com>
2022-07-09 20:00:32 +0100

    feat: export valid file mime types list

--- src/rules/files.ts
+++ src/rules/files.ts
@@ -1,33 +1,31 @@
 import { isInvalid, isValid } from '../utils/rules'
 import Mimes from './mimes.json'
 
-const videoTypes = Object.keys(Mimes).filter((mime) => mime.startsWith('video/'))
-const audioTypes = Object.keys(Mimes).filter((mime) => mime.startsWith('audio/'))
-const imageTypes = Object.keys(Mimes).filter((mime) => mime.startsWith('image/'))
+export const fileMimes = Object.keys(Mimes)
+export const videoMimeTypes = fileMimes.filter((mime) => mime.startsWith('video/'))
+export const audioMimeTypes = fileMimes.filter((mime) => mime.startsWith('audio/'))
+export const imageMimeTypes = fileMimes.filter((mime) => mime.startsWith('image/'))
 
-export const isImage = <Type> (file: Type, error = 'is not a valid image') => {
-	if (!isFile(file).valid) return isInvalid(error)
+export const isImage = <Type> (file: Type, error = 'is not a recognized image file') => {
 	//@ts-ignore
-	if (imageTypes.includes(file.type)) return isValid()
+	if (imageMimeTypes.includes(file?.type)) return isValid()
 	return isInvalid(error)
 }
 
-export const isAudio = <Type> (file: Type, error = 'is not a recognized audio extension') => {
-	if (!isFile(file).valid) return isInvalid(error)
+export const isAudio = <Type> (file: Type, error = 'is not a recognized audio file') => {
 	//@ts-ignore
-	if (audioTypes.includes(file.type)) return isValid()
+	if (audioMimeTypes.includes(file?.type)) return isValid()
 	return isInvalid(error)
 }
 
-export const isVideo = <Type> (file: Type, error = 'is not a recognized video extension') => {
-	if (!isFile(file).valid) return isInvalid(error)
+export const isVideo = <Type> (file: Type, error = 'is not a recognized video file') => {
 	//@ts-ignore
-	if (videoTypes.includes(file.type)) return isValid()
+	if (videoMimeTypes.includes(file?.type)) return isValid()
 	return isInvalid(error)
 }
 
 export const isFile = <Type> (file: Type, error = 'is not a recognized file extension') => {
 	//@ts-ignore
-	if (file?.type && Mimes[file.type]) return isValid()
+	if (fileMimes.includes(file?.type)) return isValid()
 	return isInvalid(error)
 }
