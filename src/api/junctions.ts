71d4a910eca52b4695b4ca0781424e6760336e4b
kevinand11 <kevinfizu@gmail.com>
2023-01-22 20:20:07 +0100

    fix: allowing transforms in junctions and records

--- src/api/junctions.ts
+++ src/api/junctions.ts
@@ -1,10 +1,11 @@
 import { VCore } from './core'
 import { isInvalid, isValid, makeRule } from '../utils/rules'
 
-type G1<C extends VCore<any, any>> = C extends VCore<infer T, any> ? T : unknown;
-type G2<C extends VCore<any>> = C extends VCore<any, infer T> ? T : unknown;
+type G1<C extends VCore<any>> = C extends VCore<infer T, any, any> ? T : unknown;
+type G2<C extends VCore<any>> = C extends VCore<any, infer T, any> ? T : unknown;
+type G3<C extends VCore<any>> = C extends VCore<any, any, infer T> ? T : unknown;
 
-export class VOr<T extends VCore<any, any>[]> extends VCore<G1<T[number]>, G2<T[number]>> {
+export class VOr<T extends VCore<any, any, any>[]> extends VCore<G1<T[number]>, G2<T[number]>, G3<T[number]>> {
 	constructor (options: T, err = 'doesnt match any of the schema') {
 		super()
 		this.addRule(makeRule((value) => {
@@ -18,17 +19,18 @@ export class VOr<T extends VCore<any, any>[]> extends VCore<G1<T[number]>, G2<T[
 	}
 }
 
-export class VAnd<I, O> extends VCore<I, O> {
-	constructor (options: VCore<I, O>[], err = 'doesnt match the schema') {
+export class VAnd<I, O, Tr> extends VCore<I, O, Tr> {
+	constructor (options: VCore<I, O, Tr>[], err = 'doesnt match the schema') {
 		super()
 		this.addRule(makeRule((value) => {
-			if (options.length === 0) return isValid(value)
+			let v = value as any
+			if (options.length === 0) return isValid(v)
 			for (const opt of options) {
-				const valid = opt.parse(value as any)
-				if (valid.valid) value = valid.value
-				else return isInvalid(err, value)
+				const valid = opt.parse(v)
+				if (valid.valid) v = valid.value
+				else return isInvalid(err, v)
 			}
-			return isValid(value)
+			return isValid(v)
 		}))
 	}
 }
--- src/api/records.ts
+++ src/api/records.ts
@@ -2,8 +2,8 @@ import { isInstanceOf } from '../rules'
 import { VCore } from './core'
 import { isInvalid, isValid, makeRule } from '../utils/rules'
 
-export class VMap<KI, VI, KO, VO> extends VCore<Map<KI, VI>, Map<KO, VO>> {
-	constructor (kCom: VCore<KI, KO>, vCom: VCore<VI, VO>, err?: string) {
+export class VMap<KI, VI, KO, VO, KTr, VTr> extends VCore<Map<KI, VI>, Map<KO, VO>, Map<KTr, VTr>> {
+	constructor (kCom: VCore<KI, KO, KTr>, vCom: VCore<VI, VO, VTr>, err?: string) {
 		super()
 		this.addTyping(isInstanceOf(Map, err))
 		this.addRule(makeRule((value) => {
@@ -13,7 +13,9 @@ export class VMap<KI, VI, KO, VO> extends VCore<Map<KI, VI>, Map<KO, VO>> {
 				const vValid = vCom.parse(v as any)
 				if (!kValid.valid) return isInvalid(err ?? `contains an invalid key ${String(k)}`, value)
 				if (!vValid.valid) return isInvalid(err ?? `contains an invalid value for key ${String(k)}`, value)
+				// @ts-ignore
 				if (k !== kValid.value) value.delete(k)
+				// @ts-ignore
 				value.set(kValid.value, vValid.value)
 			}
 			return isValid(value)
@@ -21,14 +23,15 @@ export class VMap<KI, VI, KO, VO> extends VCore<Map<KI, VI>, Map<KO, VO>> {
 	}
 }
 
-export class VRecord<VI, VO> extends VCore<Record<string, VI>, Record<string, VO>> {
-	constructor (vCom: VCore<VI, VO>, err?: string) {
+export class VRecord<VI, VO, VTr> extends VCore<Record<string, VI>, Record<string, VO>, Record<string, VTr>> {
+	constructor (vCom: VCore<VI, VO, VTr>, err?: string) {
 		super()
 		this.addTyping(makeRule((value) => {
 			for (const [k, v] of Object.entries(value)) {
 				const validity = vCom.parse(v as any)
 				err = err ?? `contains an invalid value for key ${k}`
 				if (!validity.valid) return isInvalid(err, value)
+				// @ts-ignore
 				value[k] = validity.value
 			}
 			return isValid(value)
