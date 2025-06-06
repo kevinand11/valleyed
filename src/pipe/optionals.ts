29a6fdc878fbc0075a6ac285a09706ac075f7b40
kevinand11 <kevinfizu@gmail.com>
2025-06-06 17:36:22 +0100

    feat: optionals api

---
+++ src/pipe/optionals.ts
@@ -0,0 +1,24 @@
+import { makePipeFn, Pipe, PipeInput, PipeOutput } from './base'
+
+const partial = <T extends Pipe<unknown, unknown>, P>(branch: T, partialCondition: (i: unknown) => boolean, force: boolean) =>
+	makePipeFn<PipeInput<T>, PipeOutput<T> | P>((input) => {
+		const isPartial = partialCondition(input)
+		if (isPartial) return input as P
+		const value = branch.safeParse(input)
+		if ('value' in value) return value.value as PipeOutput<T>
+		if (force) throw value.error
+		return input as P
+	})
+
+export const nullable = <T extends Pipe<unknown, unknown>>(branch: T) => partial<T, null>(branch, (i) => i === null, true)
+export const optional = <T extends Pipe<unknown, unknown>>(branch: T) => partial<T, undefined>(branch, (i) => i === undefined, true)
+export const requiredIf = <T extends Pipe<unknown, unknown>>(branch: T, condition: () => boolean) =>
+	partial<T, undefined>(branch, () => !condition(), condition())
+
+type FunctionOrValue<T> = T | (() => T)
+
+export const withDefault = <T extends Pipe<unknown, unknown>>(branch: T, def: FunctionOrValue<PipeInput<T>>) =>
+	makePipeFn<PipeInput<T>, Exclude<PipeOutput<T>, undefined>>((input) => {
+		const value = input !== undefined ? input : typeof def === 'function' ? (def as Function)() : def
+		return branch.parse(value) as any
+	})
